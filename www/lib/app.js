59b03b6580a218adcb0057ac0b94b7f5950013d6
Michael <lecorbeaulibre@gmail.com>
2016-05-31 18:49:27 +0300

    Update app.js

--- www/lib/app.js
+++ www/lib/app.js
@@ -24,7 +24,6 @@ module.controller('menuController', function($scope, $http, $sce) {
 		
 		//function getting classes if someone wants to join class
 		$scope.joinClass = function(){
-			alert("do stuf");
 			//localStorage.setItem("chosen_proj_id", proj_id);
 			$scope.registrationType = 'join';
 			$http({
@@ -35,7 +34,6 @@ module.controller('menuController', function($scope, $http, $sce) {
 					callback:'JSON_CALLBACK'
 				},
 			}).then(function(response) {
-				alert(response);
 				$scope.allProjTemplates = response.data;
 				menu.setMainPage('select-project.html', {closeMenu: true});
 			});
