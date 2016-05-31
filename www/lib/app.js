a778972017100199dc42662f635a09287a2c2663
Michael <lecorbeaulibre@gmail.com>
2016-05-31 23:04:53 +0300

    Update app.js

--- www/lib/app.js
+++ www/lib/app.js
@@ -31,7 +31,7 @@ module.controller('menuController', function($scope, $http, $sce) {
 			$scope.registrationType = 'join';
 			$http({
 				url: "http://www.letsgetstartup.com/app-cloud/wp-admin/admin-ajax.php", 
-				method: "get",
+				method: "jsonp",
 				//headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 				data: {
 					action: "get_proj_api",
