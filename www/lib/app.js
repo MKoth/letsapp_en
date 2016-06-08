2bcef28e60026a2f4e06f45634ecf78288febeb2
MKoth <lecorbeaulibre@gmail.com>
2016-06-09 02:47:20 +0300

    Adding get position

--- www/lib/app.js
+++ www/lib/app.js
@@ -18,18 +18,10 @@ module.controller('menuController', function($scope, $http, $sce) {
 	});*/
 		$scope.getCurrentPositionFunc = function(){
 			navigator.geolocation.getCurrentPosition($scope.geolocationSuccess,$scope.geolocationError);
-			//alert();
 		}
 		$scope.geolocationSuccess = function(position){
-			alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-		  alert();
+		  jQuery("#add-item-area input[placeholder='Longitude']").val(position.coords.longitude);
+		  jQuery("#add-item-area input[placeholder='Latitude']").val(position.coords.latitude);
 		}
 		$scope.geolocationError = function(error){
 			alert(error);
